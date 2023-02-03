0d57771c956db1d51afe6370f179f8cf01854672
david.pino <david@ip-172-31-89-46.ec2.internal>
2023-02-03 17:58:56 +0000

    build: creacion asset linechart - horizontalchart

---
+++ sym-lineschart.js
@@ -0,0 +1,334 @@
+(function (BS) {
+  function symbolVis() {}
+  BS.deriveVisualizationFromBase(symbolVis);
+
+  var definition = {
+    typeName: "lineschart",
+    displayName: "Grafico de lineas",
+    inject: ["timeProvider"],
+    datasourceBehavior: BS.Extensibility.Enums.DatasourceBehaviors.Multiple,
+    iconUrl: "/Scripts/app/editor/symbols/ext/icons/barsVerticalCOMM.png",
+    visObjectType: symbolVis,
+
+    getDefaultConfig: function () {
+      return {
+        DataShape: "TimeSeries",
+        FormatType: null,
+        Height: 500,
+        Width: 500,
+        fontSize: 22,
+        textColor1: "black",
+        textColor2: "#000000",
+        colorfill2: "#FC0000",
+        backgroundColor: "transparent",
+        outlineColor: "white",
+        useCustomTitle: false,
+        customTitle: "",
+        showLegend: false,
+        fontSizeInside: 12,
+        labels: [],
+        units: [],
+        decimalPlaces: 0,
+        href: "",
+      };
+    },
+
+    configOptions: function () {
+      return [
+        {
+          title: "Format Symbol",
+          mode: "format",
+        },
+      ];
+    },
+  };
+
+  symbolVis.prototype.init = function (scope, elem, timeProvider) {
+    console.log("\t[+]Diagrama Paretto");
+    scope.config.FormatType = null;
+    this.onDataUpdate = myCustomDataUpdateFunction;
+    this.onConfigChange = myCustomConfigurationChangeFunction;
+    var symbolContainerDiv = elem.find("#container")[0];
+    var newUniqueIDString = "amChart_" + scope.symbol.Name;
+    symbolContainerDiv.id = newUniqueIDString;
+
+    var chart;
+    var dataArray;
+    var graphArray;
+    var titleArray;
+    var isFirts = true;
+
+    // Funcion inicilizadora
+    function myCustomDataUpdateFunction(data) {
+      console.log(
+        " ~ file: sym-parettochart.js:59 ~ myCustomDataUpdateFunction ~ data",
+        data
+      );
+      dataArray = [];
+      if (data) {
+        fillDataArray(data);
+        fillTitleArray(data);
+        fillGraphArray(data);
+
+        if (!chart) chart = getNewChart(dataArray, graphArray);
+        else refreshChart(chart, dataArray, graphArray);
+      } else {
+        getNewChart(dataArray, graphArray);
+      }
+    }
+
+    function fillDataArray(data) {
+      let lastDate =
+        timeProvider.displayTime.end != "*"
+          ? new Date(timeProvider.displayTime.end)
+          : new Date();
+      let startDate = new Date(timeProvider.displayTime.start);
+      let iterableDate = startDate;
+      let daysOfMonth = getDaysOfMonth(lastDate.getMonth() + 1);
+      let daysOfPreviewMonth = getDaysOfMonth(startDate.getMonth() + 1);
+      let moreDays = null;
+
+      moreDays = daysOfPreviewMonth - startDate.getDate();
+      lastDate.setDate(lastDate.getDate() + 1);
+
+      for (let dayIndex = 1; dayIndex <= daysOfMonth + moreDays; dayIndex++) {
+        iterableDate.setDate(iterableDate.getDate() + 1);
+
+        if (iterableDate.getTime() <= lastDate.getTime()) {
+          // One
+          let arrayValuesOne = data.Data[0].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesOne = arrayValuesOne.filter(el => el != 'Calc Failed');
+          let maxValueOne = (arrayValuesOne.length > 0) ? Math.max(...arrayValuesOne) : 0;
+          // Two
+          let arrayValuesTwo = (!data.Data[1]) ? [] : data.Data[1].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesTwo = arrayValuesTwo.filter(el => el != 'Calc Failed');
+          let maxValueTwo = (!data.Data[1]) ? 0 : ((arrayValuesTwo.length > 0) ? Math.max(...arrayValuesTwo) : 0);
+          // Three
+          let arrayValuesThree = (!data.Data[2]) ? [] : data.Data[2].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesThree = arrayValuesThree.filter(el => el != 'Calc Failed');
+          let maxValueThree = (!data.Data[2]) ? 0 : ((arrayValuesThree.length > 0) ? Math.max(...arrayValuesThree) : 0);
+          // Four
+          let arrayValuesFour = (!data.Data[3]) ? [] : data.Data[3].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesFour = arrayValuesFour.filter(el => el != 'Calc Failed');
+          let maxValueFour = (!data.Data[3]) ? 0 : ((arrayValuesFour.length > 0) ? Math.max(...arrayValuesFour) : 0);
+          // Five
+          let arrayValuesFive = (!data.Data[4]) ? [] : data.Data[4].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesFive = arrayValuesFive.filter(el => el != 'Calc Failed');
+          let maxValueFive = (!data.Data[4]) ? 0 : ((arrayValuesFive.length > 0) ? Math.max(...arrayValuesFive) : 0);
+          // Six
+          let arrayValuesSix = (!data.Data[5]) ? [] : data.Data[5].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesSix = arrayValuesSix.filter(el => el != 'Calc Failed');
+          let maxValueSix = (!data.Data[5]) ? 0 : ((arrayValuesSix.length > 0) ? Math.max(...arrayValuesSix) : 0);
+          // Seven
+          let arrayValuesSeven = (!data.Data[6]) ? [] : data.Data[6].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesSeven = arrayValuesSeven.filter(el => el != 'Calc Failed');
+          let maxValueSeven = (!data.Data[6]) ? 0 : ((arrayValuesSeven.length > 0) ? Math.max(...arrayValuesSeven) : 0);
+          // Eight
+          let arrayValuesEight = (!data.Data[7]) ? [] : data.Data[6].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesEight = arrayValuesEight.filter(el => el != 'Calc Failed');
+          let maxValueEight = (!data.Data[7]) ? 0 : ((arrayValuesEight.length > 0) ? Math.max(...arrayValuesEight) : 0);
+          // Nine
+          let arrayValuesNine = (!data.Data[8]) ? [] : data.Data[6].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesNine = arrayValuesNine.filter(el => el != 'Calc Failed');
+          let maxValueNine = (!data.Data[8]) ? 0 : ((arrayValuesNine.length > 0) ? Math.max(...arrayValuesNine) : 0);          
+          // Ten
+          let arrayValuesTen = (!data.Data[9]) ? [] : data.Data[6].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesTen = arrayValuesTen.filter(el => el != 'Calc Failed');
+          let maxValueTen = (!data.Data[9]) ? 0 : ((arrayValuesTen.length > 0) ? Math.max(...arrayValuesTen) : 0);
+          // Eleven
+          let arrayValuesEleven = (!data.Data[10]) ? [] : data.Data[10].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesEleven = arrayValuesEleven.filter(el => el != 'Calc Failed');
+          let maxValueEleven = (!data.Data[10]) ? 0 : ((arrayValuesEleven.length > 0) ? Math.max(...arrayValuesEleven) : 0);
+          // Twelve
+          let arrayValuesTwelve = (!data.Data[11]) ? [] : data.Data[11].Values.filter(el => el.Time.includes(formatDate(iterableDate))).map(el => el.Value);
+          arrayValuesTwelve = arrayValuesTwelve.filter(el => el != 'Calc Failed');
+          let maxValueTwelve = (!data.Data[11]) ? 0 : ((arrayValuesTwelve.length > 0) ? Math.max(...arrayValuesTwelve) : 0);
+
+          let newDataObject = getNewDataObject(formatDate(iterableDate), maxValueOne, maxValueTwo, maxValueThree, maxValueFour, maxValueFive, maxValueSix, maxValueSeven, maxValueEight, maxValueNine, maxValueTen, maxValueEleven, maxValueTwelve);
+          dataArray.push(newDataObject);
+        } else {
+          let newDataObject = getNewDataObject(formatDate(iterableDate), null, null, null, null, null, null, null, null, null, null, null, null);
+          dataArray.push(newDataObject);
+        }
+      }
+    }
+
+    function fillTitleArray ( data ) {
+      titleArray = [];
+      titleArray = data.Data.map( el => el.Label.split('|')[1] );
+    }
+
+    function fillGraphArray ( data ) {
+      graphArray = [];
+      for (let index = 1; index <= data.Data.length; index++) {
+        
+        graphArray.push({
+          id: `g${index}`,
+          balloon: {
+            drop: true,
+            adjustBorderColor: false,
+            color: "#ffffff",
+          },
+          bullet: "round",
+          bulletBorderAlpha: 1,
+          bulletColor: "#FFFFFF",
+          bulletSize: 5,
+          hideBulletsCount: 50,
+          lineThickness: 2,
+          title: `${titleArray[index - 1]}`,
+          useLineColorForBulletBorder: true,
+          valueField: `value${index}`,
+          balloonText: `<span style='font-size:18px;'>[[value${index}]]</span>`,
+          valueAxis: "v1",
+        })
+
+      }
+    }
+
+    function getDaysOfMonth(numMonth, numYear) {
+      let daysOfMonth = 31;
+      numMonth = parseInt(numMonth);
+      numYear = parseInt(numYear);
+      if (numMonth == 4 || numMonth == 6 || numMonth == 9 || numMonth == 11) {
+        daysOfMonth = 30;
+      }
+      if (numMonth == 2) {
+        daysOfMonth = 28;
+        if (numYear % 4 == 0) {
+          daysOfMonth = 29;
+        }
+      }
+      return daysOfMonth;
+    }
+
+    function formatDate(date) {
+      return date.toISOString().split("T")[0];
+    }
+
+    function getNewDataObject( date, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, value11, value12 ){
+      return {
+        date,
+        value1,
+        value2,
+        value3,
+        value4,
+        value5,
+        value6,
+        value7,
+        value8,
+        value9,
+        value10,
+        value11,
+        value12,
+      }
+    }
+
+    // Funcion invocadora del grafico
+    function getNewChart(dataArray, graphArray) {
+      return AmCharts.makeChart(symbolContainerDiv.id, {
+        type: "serial",
+        theme: "none",
+        marginRight: 40,
+        marginLeft: 40,
+        hideCredits: true,
+        autoMarginOffset: 20,
+        dataDateFormat: "YYYY-MM-DD",
+        valueAxes: [
+          {
+            id: "v1",
+            axisAlpha: 0,
+            position: "left",
+            ignoreAxisWidth: true,
+          },
+        ],
+        balloon: {
+          borderThickness: 1,
+          shadowAlpha: 0,
+        },
+        graphs: graphArray,
+        categoryField: "date",
+        categoryAxis: {
+          dashLength: 1,
+          minorGridEnabled: true,
+        },
+        dataProvider: dataArray,
+        legend: {
+          equalWidths: false,
+          enabled: true,
+          valueAlign: "right",
+          horizontalGap: 10,
+          useGraphSettings: true,
+          bold: true,
+          markerSize: 24,
+        },
+      });
+    }
+
+    // Funcion refresco del grafico
+    function refreshChart(chart, dataArray, graphArray) {
+      chart.titles = createArrayOfChartTitles();
+      chart.dataProvider = dataArray;
+      chart.graphs = graphArray;
+      chart.validateData();
+      chart.validateNow();
+    }
+
+    function createArrayOfChartTitles() {
+      var titlesArray;
+      if (scope.config.useCustomTitle) {
+        titlesArray = [
+          {
+            text: scope.config.customTitle,
+            size: scope.config.fontSize + 5,
+          },
+        ];
+      }
+      return titlesArray;
+    }
+
+    // Funcion de configuracion de estilos
+    function myCustomConfigurationChangeFunction(data) {
+      if (chart) {
+        // if (chart.fontSize !== scope.config.fontSize) {
+          // chart.titles = createArrayOfChartTitles();
+          // chart.fontSize = scope.config.fontSize;
+          // chart.graphs[0].fontSize = scope.config.fontSize + 10;
+          // chart.graphs[1].fontSize = scope.config.fontSize + 10;
+          // chart.legend.fontSize = scope.config.fontSize;
+        // }
+
+        // if (chart.graphs[1].precision != scope.config.decimalPlaces) {
+        //   chart.graphs[1].precision = scope.config.decimalPlaces;
+        // }
+
+        // if (chart.graphs[0].color !== scope.config.textColor1) {
+        //   chart.graphs[0].color = scope.config.textColor1;
+        //   chart.legend.color = scope.config.textColor1;
+        // }
+
+        // if (chart.graphs[1].color !== scope.config.textColor2) {
+        //   chart.graphs[1].color = scope.config.textColor2;
+        // }
+
+        // if (chart.colors[1] != scope.config.colorfill2) {
+        //   chart.colors[1] = scope.config.colorfill2;
+        // }
+
+        // if (chart.backgroundColor !== scope.config.backgroundColor)
+        //   chart.backgroundColor = scope.config.backgroundColor;
+
+        if (scope.config.useCustomTitle)
+          chart.titles = createArrayOfChartTitles();
+        else chart.titles = null;
+
+        chart.legend.enabled = scope.config.showLegend;
+
+        chart.validateData();
+        chart.validateNow();
+      }
+    }
+  };
+
+  BS.symbolCatalog.register(definition);
+})(window.PIVisualization);
+
